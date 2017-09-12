fd57634704ade52f44ad12e2fc7c8f79e3f98f9c
mauriciocarazas <mauriciocarazas9812@gmail.com>
2017-09-12 06:25:00 -0500

    lab04

---
+++ lab 04  en clase/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+//definimos el for
+/*
+int funtion_for(int i=0 ,int a){
+    if(i==a){
+        return 0;
+    }
+    else{
+        cout << i << endl;
+        return funtion (++i,a);
+    }
+}*/
+int main()
+{
+   /* //1 definimos el for
+    funtion_for(0,4);
+*/
+//HACER UNA FUNCION QUE IMPRIMA LOS NUMEROS PRIMOS MENORES A N
+    int maxa;
+    cout << "Dame el limite de la lista" << endl;
+    cin >> maxa;
+    for (int i=2 ; i < maxa ; i++){
+        bool isPrime = true;
+        for (int j=2 ; j<1 ; j++){
+            ig
+        }
+    // HACER UNA FUNCION QUE INDIQUE QUE NUMERO PRIMO ES N
+    // POR EJEMPLO SI YO PONGO 8 ME DARIA EL OCTAVO PRIMO
+return 0;
+}
